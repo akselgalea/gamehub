654bd7d131c9568a44cb5d047b5568944598deb3
Johann Muñoz <practicas@informaticapucv.cl>
2023-06-12 16:44:35 -0400

    Migraciones de instancias

---
+++ database/migrations/2023_06_12_202344_create_game_instance_scores.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('game_instance_scores', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('max_score')->unsigned()->default(0);
+            $table->foreignId('user_id');
+            $table->foreignId('game_instance_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('game_instance_scores');
+    }
+};
