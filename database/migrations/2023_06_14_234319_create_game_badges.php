cdbae5d409e2aff1101f3925ca8e8a8010ba4e5e
Johann Muñoz <practicas@informaticapucv.cl>
2023-06-14 20:17:00 -0400

    Migracion de game badges y game user badges

---
+++ database/migrations/2023_06_14_234319_create_game_badges.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('game_badges', function (Blueprint $table) {
+            $table->id();
+            $table->increments('id');
+            $table->string('code');
+            $table->string('name');
+            $table->string('description');
+            $table->string('conditions');
+            $table->string('var')->nullable();
+            $table->string('var_to_ref')->nullable();
+            $table->integer('target');
+            $table->string('type');
+            $table->string('image');
+            $table->foreignId('game_id')->nullable()->nullOnDelete();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('game_badges');
+    }
+};
